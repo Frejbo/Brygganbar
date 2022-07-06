590852cd74966452861e2453cb180d980fa77367
Frejbo <frej.hellsten@gmail.com>
2022-07-06 23:47:06 +0200

    Scrollvideor börjar inte tills de är inom viewport

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const canvas = document.querySelector(".kulglasskopning");
 const context = canvas.getContext("2d");
 
 const currentFrame = index => (
-    ("Glasskopning/glasskopning" + index.toString().padStart(4, "0") + ".jpg")
+    ("Glasskopning/img" + index.toString().padStart(4, "0") + ".jpg")
 )
 
-const frameCount = 376;
-
+const frameCount = 151;
 canvas.width = 640;
 canvas.height = 360;
 
@@ -16,17 +15,14 @@ img.src = currentFrame(1);
 img.onload = function() {
     context.drawImage(img, 0, 0)
 }
-
 const updateImage = index => {
     img.src = currentFrame(index)
     context.drawImage(img, 0, 0)
 }
-
 window.addEventListener("scroll", () => {
-    const scrollTop = html.scrollTop
-    const maxScroll = html.scrollHeight - window.innerHeight
-    const scrollFraction = scrollTop / maxScroll
+    const scrollTop = -document.querySelector('.kulglasskopning').getBoundingClientRect().bottom + (window.innerHeight*.95)
+    const maxScroll = window.innerHeight*.5
+    const scrollFraction = (scrollTop / maxScroll)
     const frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount))
-
-    requestAnimationFrame( () => updateImage(frameIndex))
+    requestAnimationFrame( () => updateImage(frameIndex + 1))
 })
