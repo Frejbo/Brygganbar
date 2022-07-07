b0682890bee92bc08051c34f991397418e702a72
Frejbo <frej.hellsten@gmail.com>
2022-07-07 22:27:48 +0200

    Möjliggör nu flera scrollvideor
    Konfigurera main.JS variablerna i toppen för att implementera en ny.

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
 const html = document.documentElement;
-const canvas = document.querySelector(".kulglasskopning");
-const context = canvas.getContext("2d");
 
-const currentFrame = index => (
-    ("Glasskopning/img" + index.toString().padStart(4, "0") + ".jpg")
-)
+const glasscanvas = document.querySelector(".kulglasskopning");
+const pizzacanvas = document.querySelector(".pizzavideo");
 
-const frameCount = 151;
-canvas.width = 640;
-canvas.height = 360;
+const glass_frameCount = 151;
+const pizza_frameCount = 118;
 
-const img = new Image();
-img.src = currentFrame(1);
-img.onload = function() {
-    context.drawImage(img, 0, 0)
+const glass_img = new Image();
+const pizza_img = new Image();
+
+glass_img.src = "Glasskopning/img0001.jpg";
+glass_img.onload = function() {
+    glasscanvas.getContext("2d").drawImage(glass_img, 0, 0);
 }
-const updateImage = index => {
-    img.src = currentFrame(index)
-    context.drawImage(img, 0, 0)
+pizza_img.src = "pizzavideo/img0001.jpg";
+pizza_img.onload = function() {
+    pizzacanvas.getContext("2d").drawImage(pizza_img, 0, 0);
 }
+
+glasscanvas.width = 640; glasscanvas.height = 360;
+pizzacanvas.width = 640; pizzacanvas.height = 360;
+
+const element_names = [[glasscanvas, "Glasskopning/img", glass_frameCount, glass_img, ".kulglasskopning"], [pizzacanvas, "pizzavideo/img", pizza_frameCount, pizza_img, ".pizzavideo"]]
+
+
+
+
 window.addEventListener("scroll", () => {
-    const scrollTop = -document.querySelector('.kulglasskopning').getBoundingClientRect().bottom + (window.innerHeight*.95)
-    const maxScroll = window.innerHeight*.5
-    const scrollFraction = (scrollTop / maxScroll)
-    const frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount))
-    requestAnimationFrame( () => updateImage(frameIndex + 1))
+    for (let i = 0; i < element_names.length; i++) {
+        update_canvas(element_names[i][0], element_names[i][1], element_names[i][2], element_names[i][3], element_names[i][4])
+    }
 })
+
+function updateImage(index, img, canvas, path) {
+    img.src = path + index.toString().padStart(4, "0") + ".jpg";
+    canvas.getContext("2d").drawImage(img, 0, 0)
+}
+
+function update_canvas(canvas, path, frameCount, img, elementname) {
+
+    var scrollTop = -document.querySelector(elementname).getBoundingClientRect().bottom + (window.innerHeight*.95)
+    var maxScroll = window.innerHeight*.5
+    var scrollFraction = (scrollTop / maxScroll)
+    var frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount))
+    if (frameIndex >= 0) {
+        requestAnimationFrame( () => updateImage(frameIndex + 1, img, canvas, path))
+    }
+}
+
+
+
+
+// Preload
+// const preloadImages = () => {
+//     for (let i = 0; i < element_names.length; i++) {
+//         for (let i2 = 1; i2 < element_names[i][2]; i2++) {
+//             const img = new Image();
+//             img.src = (element_names[i][1] + i2.toString().padStart(4, "0") + ".jpg");
+//         }
+//     }
+// }
+// preloadImages();
